d84e37fbc8ff9404a8a4b2952433db13ef1c3585
Nick Solonyy <nicksolony@gmail.com>
2019-12-27 05:28:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if @side_a < 0 || @side_b < 0 || @side_c < 0 || (@side_a+@side_b) < @side_c || (@side_b+@side_c)<@side_a || (@side_c +@side_a) < @side_b
     raise TriangleError
     else
-      case @side_a,@side_b,@side_c
+      case self
         
         when @side_a == @side_b && @side_b == @side_c
           :equilateral
