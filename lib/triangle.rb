90946d2dcfcf97c0405fdd3235250a71c0758076
Nick Solonyy <nicksolony@gmail.com>
2019-12-27 05:21:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
   end
   
   def kind
+    @side_a < 0 || @side_b < 0 || @side_c < 0 || (@side_a+@side_b) < @side_c || (@side_b+@side_c)<@side_a || (@side_c +@side_a) < @side_b
     class TriangleError < StandardError
     # triangle error code
     end
